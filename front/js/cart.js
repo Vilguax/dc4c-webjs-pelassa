3922fdd97a0e04ddf418d43a30f6d2a02e3af1c4
Vilguax <axel.pelassa@hotmail.fr>
2023-06-15 15:42:56 +0200

    Ajout cart.js, calcul des prix et btn suppr

---
+++ front/js/cart.js
@@ -0,0 +1,59 @@
+let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart || !Array.isArray(cart) || cart.length === 0) {
+    document.getElementById('cart__items').innerHTML = '<p>Votre panier est vide.</p>';
+} else {
+    let total = 0;
+    let totalQuantity = 0;
+
+    for (let i = 0; i < cart.length; i++) {
+        const product = cart[i];
+
+        fetch(`http://localhost:3000/api/products/${product.id}`)
+            .then(response => response.json())
+            .then(productDetails => {
+                const productTotal = productDetails.price * product.quantity;
+                total += productTotal;
+                totalQuantity += product.quantity;
+
+                const productElement = document.createElement('article');
+                productElement.classList.add('cart__item');
+                productElement.dataset.id = product.id;
+                productElement.dataset.color = product.selectedColor;
+                productElement.innerHTML = `
+                    <div class="cart__item__img">
+                        <img src="${productDetails.imageUrl}" alt="${productDetails.altTxt}">
+                    </div>
+                    <div class="cart__item__content">
+                        <div class="cart__item__content__description">
+                            <h2>${productDetails.name}</h2>
+                            <p>${product.selectedColor}</p>
+                            <p>${productTotal.toFixed(2)} €</p>
+                        </div>
+                        <div class="cart__item__content__settings">
+                            <div class="cart__item__content__settings__quantity">
+                                <p>Qté : </p>
+                                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+                            </div>
+                            <div class="cart__item__content__settings__delete">
+                                <p class="deleteItem">Supprimer</p>
+                            </div>
+                        </div>
+                    </div>
+                `;
+
+                document.getElementById('cart__items').appendChild(productElement);
+                productElement.querySelector('.deleteItem').addEventListener('click', () => {
+                    cart = cart.filter(item => item.id !== product.id || item.selectedColor !== product.selectedColor);
+                    localStorage.setItem('cart', JSON.stringify(cart));
+                    productElement.remove();
+                    total -= productTotal;
+                    totalQuantity -= product.quantity;
+                    document.getElementById('totalPrice').textContent = total.toFixed(2);
+                    document.getElementById('totalQuantity').textContent = totalQuantity;
+                });
+                document.getElementById('totalPrice').textContent = total.toFixed(2);
+                document.getElementById('totalQuantity').textContent = totalQuantity;
+            });
+    }
+}
