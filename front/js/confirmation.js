5b0400d682d46474ec465c0fa415009a3bac66ea
Vilguax <axel.pelassa@hotmail.fr>
2023-06-15 16:13:01 +0200

    fix

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -2,4 +2,3 @@ const urlParams = new URLSearchParams(window.location.search);
 const orderId = urlParams.get('id');
 
 document.getElementById('orderId').textContent = orderId;
-localStorage.removeItem('cart');
