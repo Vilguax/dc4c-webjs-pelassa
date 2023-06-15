7e100f8cd4aa72630467a719896f1cd3056f5732
Vilguax <axel.pelassa@hotmail.fr>
2023-06-15 16:12:35 +0200

    remove cart du storage

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -2,3 +2,4 @@ const urlParams = new URLSearchParams(window.location.search);
 const orderId = urlParams.get('id');
 
 document.getElementById('orderId').textContent = orderId;
+localStorage.removeItem('cart');
